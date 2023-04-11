ecac4e5d0be46e4942ab4ff1b554f94412b4ce74
panminhai <e1356789@gmail.com>
2023-04-11 11:38:30 +0800

    add Bank entity

---
+++ src/main/java/com/example/java_demo_test/entity/Bank.java
@@ -0,0 +1,22 @@
+package com.example.java_demo_test.entity;
+
+public class Bank {
+	private int amount;
+	private int balance;
+	
+	
+	public int getAmount() {
+		return amount;
+	}
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	public int getBalance() {
+		return balance;
+	}
+	public void setBalance(int balance) {
+		this.balance = balance;
+	}
+	
+	
+}
