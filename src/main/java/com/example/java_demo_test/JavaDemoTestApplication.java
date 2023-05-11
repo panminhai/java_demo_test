efdc913fc4a1be8a76f4016d6df84ca1866d11fd
panminhai <e1356789@gmail.com>
2023-05-11 10:42:20 +0800

    tttt

--- src/main/java/com/example/java_demo_test/JavaDemoTestApplication.java
+++ src/main/java/com/example/java_demo_test/JavaDemoTestApplication.java
@@ -2,10 +2,13 @@ package com.example.java_demo_test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/*
+ * 実行テスト(プロジェクト動作確認)
+ */
 @SpringBootApplication
 public class JavaDemoTestApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(JavaDemoTestApplication.class, args);
 	}
